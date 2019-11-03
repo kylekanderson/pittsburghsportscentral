5f30d3aba729d7c39d320a3d47d0db8efcd49055
Kyle Anderson <kylekandderson@gmail.com>
2019-11-03 17:42:52 -0500

    added schedule data and cleaner.py to clean data

---
+++ cleaner.py
@@ -0,0 +1,13 @@
+import json
+
+with open('./public/data/Penguins/2018.json') as data_file:
+    data = json.load(data_file)
+
+itemsToRemove = []
+
+for element in reversed(data):
+    if element['GP'] == 'GP':
+        data.remove(element)
+
+with open('data.json', 'w') as data_file:
+    data = json.dump(data, data_file)
